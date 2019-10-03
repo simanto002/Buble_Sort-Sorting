84bbad320565d687549755b87900e1b0b79e8cf4
simanto002 <56089863+simanto002@users.noreply.github.com>
2019-10-03 22:42:22 +0600

    Buble Sort 1.0

---
+++ Buble Sort.py
@@ -0,0 +1,11 @@
+def sort(nums):
+  for i in range (len(nums)-1,0,-1):
+    for j in range(i):
+      if nums[j]>nums[j+1]:
+        tem=nums[j]
+        nums[j]=nums[j+1]
+        nums[j+1]=tem
+      print(nums)
+nums=[4,41,24,25,26,87,14,58,32]
+sort(nums)
+print(nums)
